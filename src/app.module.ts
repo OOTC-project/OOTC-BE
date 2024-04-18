2f490a7c58ba69c4fa9417dda51bb4e71683668a
ifLov <hyunTae.l@21market.kr>
2024-04-18 12:28:02 +0900

    [ADD] docker 로 localhost 실행을 위한 초기 세팅

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './member/member.module';
-import { PrismaModule } from './prisma/prisma.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
     imports: [
@@ -11,7 +11,7 @@ import { ConfigModule } from '@nestjs/config';
         PrismaModule,
         ConfigModule.forRoot({
             // envFilePath: `config/.env.${process.env.NODE_ENV}`,
-            envFilePath: `.env`,
+            envFilePath: [`.env`],
             isGlobal: true,
             // validationSchema: Joi.object({
             // MYSQL_HOST: Joi.string().required(),
--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
-import { ConfigService } from '@nestjs/config';
 
 @Module({
-    imports: [ConfigService],
     providers: [PrismaService],
     exports: [PrismaService],
 })
