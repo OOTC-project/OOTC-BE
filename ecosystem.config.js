3857c341f638f102e53c77b5c680d7a43dfa8f4a
현태 <hyuntae.l@21market.kr>
2024-05-17 17:27:50 +0900

    [ADD] pm2 용 스크립트 및 설정 파일

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    apps: [
+        {
+            name: 'OOTC-prod',
+            script: './dist/main.js',
+            instances: -1,
+            autorestart: false,
+            watch: false,
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+        {
+            name: 'OOTC-development',
+            script: './dist/main.js',
+            instances: 1,
+            autorestart: false,
+            watch: false,
+            env: {
+                NODE_ENV: 'debug',
+            },
+        },
+    ],
+};
