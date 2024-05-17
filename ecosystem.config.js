19d95826da9e97da937ce4ec9d2bfd87c8d567a1
현태 <hyuntae.l@21market.kr>
2024-05-17 17:36:16 +0900

    [ADD] pm2 용 스크립트 및 설정 파일

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,14 @@
 module.exports = {
     apps: [
         {
-            name: 'OOTC-prod',
-            script: './dist/main.js',
-            instances: -1,
-            autorestart: false,
-            watch: false,
+            name: 'your-app-name',
+            script: 'node_modules/.bin/nest',
+            args: 'start',
             node_args: '--max-old-space-size=4096',
+            instances: 1,
+            autorestart: true,
+            watch: false,
             max_memory_restart: '1G',
-            env: {
-                NODE_ENV: 'production',
-            },
         },
     ],
 };
